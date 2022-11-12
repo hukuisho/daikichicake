6cbf89e50b352b7df2ef9befc78c5c27ab0722af
sho0322 <infinity0322s@gmail.com>
2022-11-12 16:43:58 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
