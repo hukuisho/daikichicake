c16160f87af43da1300ff7cabcc6f2db18ca64fc
sho0322 <infinity0322s@gmail.com>
2022-11-17 17:09:27 +0900

    add:common_scss

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,31 @@
   <router-view />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+// keyframes
+@keyframes flash {
+  from {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+// mixin
+@mixin textShadowColor($textShadowColor) {
+  text-shadow: $color 1px 0px 1px, $textShadowColor -1px 0px 1px,
+    $textShadowColor -1px 0px 1px, $textShadowColor -1px 0px 1px,
+    $textShadowColor 1px 1px 1px, $textShadowColor -1px 1px 1px,
+    $textShadowColor 1px -1px 1px, $textShadowColor -1px -1px 1px,
+    $textShadowColor 1px 1px 1px, $textShadowColor -1px 1px 1px;
+}
+@mixin boxShadowColor($boxShadowColor) {
+  box-shadow: 0px 0px $boxShadowColor;
+}
+@mixin gradientColor($direction, $colorFirst, $colorSecond) {
+  background-image: linear-gradient($direction, $colorFirst, $colorSecond);
+}
+</style>
