56ab671b7c73b58bb88b626eb7cca8265d94f7fd
sho0322 <infinity0322s@gmail.com>
2022-11-18 11:37:48 +0900

    fix:scss

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,41 @@
   <router-view />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  width: 100%;
+  margin: 0;
+  font-family: serif;
+  font-weight: bold;
+  box-sizing: border-box;
+}
+body {
+  width: 100%;
+  min-height: 100%;
+  margin: 0 auto;
+  text-align: center;
+  color: #000;
+  object-fit: contain;
+  overflow: hidden;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+a {
+  text-decoration: none;
+}
+// keyframes
+@keyframes flash {
+  from {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,3 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-module.exports = {
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: '@import "./src/assets/sass/common.scss"',
-      },
-    },
-  },
-};
