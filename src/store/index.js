94bb78566f08a83c865db6190ed5391e491fb0ea
sho0322 <infinity0322s@gmail.com>
2022-11-19 14:49:08 +0900

    GetView

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,19 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    //opening
     openingTitle: "DaikitchiCake",
     openingSubTitle: "Hope You Have a Wonderful Day!!!",
     openingWelcome: "Welcome To Store",
+    //購入成功
+    getThanks: "ご注文ありがとうございました🍴",
+    getDate: "11月11日11時",
+    getExplanationMessageFirst: "DaikitchiCake(木星店)にてお待ちしております。",
+    getExplanationMessageSecond:
+      "当日、現地で会計を済ませて商品をお受け取り下さい。",
+    getLinkToHistory: "注文した商品はコチラから購入履歴をご確認下さい。",
+    getNote: "受け取り時間変更はコチラまでご連絡下さい↓",
+    getPhoneNumber: "AAA-BBB-CCC",
   },
   getters: {},
   mutations: {},
--- src/views/shop/product/GetView.vue
+++ src/views/shop/product/GetView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div id="product__get">
+  <div id="product__get" class="product__get">
+    <h5 class="product__get__thanks">{{ $store.state.getThanks }}</h5>
+    <div class="product__get__explanation">
+      <h1 class="product__get__explanation__date">
+        <!-- コチラ後ほど予約日に改修 -->
+        {{ $store.state.getDate }}
+      </h1>
+      <h5 class="product__get__explanation__message">
+        {{ $store.state.getExplanationMessageFirst }}
+      </h5>
+      <h5 class="product__get__explanation__message">
+        {{ $store.state.getExplanationMessageSecond }}
+      </h5>
+      <router-link to="/shop/user/history">
+        <h5 class="product__get__explanation__link--to--history">
+          {{ $store.state.getLinkToHistory }}
+        </h5>
+      </router-link>
+      <h5 class="product__get__explanation__note">
+        {{ $store.state.getNote }}
+      </h5>
+      <h5 class="product__get__explanation__note">
+        {{ $store.state.getPhoneNumber }}
+      </h5>
+    </div>
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
@@ -16,3 +40,31 @@
 //   },
 // };
 </script>
+<style lang="scss">
+.product__get {
+  background-color: antiquewhite;
+  min-height: 100vh;
+  &__thanks {
+    background-color: orange;
+  }
+  &__explanation {
+    &__date {
+      background-color: #ffff;
+      padding: 15% 0;
+      margin: 10% 0 10%;
+      text-decoration: underline 3px palevioletred solid;
+    }
+    &__message {
+      margin: 2% 0;
+    }
+    &__link--to--history {
+      padding: 5% 0;
+      text-decoration: underline 1.5px blue solid;
+    }
+    &__note {
+      color: red;
+      text-decoration: underline 2px red dotted;
+    }
+  }
+}
+</style>
